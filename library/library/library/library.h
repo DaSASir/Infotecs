893ac073618830e23a10c4fc0f466560be0c6bb1
DaSASir <sapukov1701@mail.ru>
2026-07-17 14:09:48 +0700

    library
    реализована динамическая библиотека и настроена сборка CMake

---
+++ library/library/library/library.h
@@ -0,0 +1,17 @@
+#ifndef LIBRARY_H
+#define LIBRARY_H
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+//function number 1
+void sortAndReplace(std::string& input);
+
+//function number 2
+int getSum(const std::string& input);
+
+//function number 3
+bool analyseOfSymbols(const std::string& input);
+
+#endif // LIBRARY_H
